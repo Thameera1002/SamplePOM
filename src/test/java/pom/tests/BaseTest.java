d78d17ae43806553ae1437ab981fc0ce8bf0ae77
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-10-25 15:42:30 +0530

    BaseTest.java updated

--- src/test/java/pom/tests/BaseTest.java
+++ src/test/java/pom/tests/BaseTest.java
@@ -4,19 +4,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pom.util.BrowserFactory;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
+    public BrowserFactory browserFactory = BrowserFactory.getInstance();
+
     @BeforeMethod
     public void beforeMethod(){
-        driver = WebDriverManager.chromedriver().create();
-        driver.manage().window().maximize();
+//        driver = WebDriverManager.chromedriver().create();
+//        driver.manage().window().maximize();
+        driver = browserFactory.getDriver();
     }
 
     @AfterMethod
     public void afterMethod(){
-        driver.quit();
+//        driver.quit();
+        browserFactory.quitDriver();
     }
 }
